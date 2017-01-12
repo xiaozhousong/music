68955e52135518862d15432de69784990bd1064e
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-12 07:35:27 +0000

    add a view for detail page

--- music/views.py
+++ music/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("<h1>This is the music app homepage</h1>")
+
+def detail(request, album_id):
+	return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
