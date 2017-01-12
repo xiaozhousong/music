4df84c903bb92bf798e162c9f1081d9a7cc9b2da
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-12 08:46:32 +0000

    add detail template, and import HTTP404, so when user try to get the album which is not in the database, send the url to 404 page

--- music/views.py
+++ music/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from .models import Album
-from django.template import loader
+#from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
 	all_albums = Album.objects.all()
-	template = loader.get_template('music/index.html')
-	context = {
-	   'all_albums' : all_albums,
-	}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-	return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+	try:
+		album = Album.objects.get(pk =  album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album does not exist")
+	return render(request, 'music/detail.html', {'album' : album})
