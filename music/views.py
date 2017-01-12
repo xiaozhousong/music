3ba941114c264af2c56636fe7c68ed1e5bfc2bee
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-12 08:14:25 +0000

    set up html templates

--- music/views.py
+++ music/views.py
@@ -4,16 +4,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Album
+from django.template import loader
 
 def index(request):
 	all_albums = Album.objects.all()
-
-	html = ''
-
-	for album in all_albums:
-		url = '/music/' + str(album.id) + '/'
-		html += '<a href=" ' + url + ' ">' + album.album_title + '</a><br>'
-	return HttpResponse(html)
+	template = loader.get_template('music/index.html')
+	context = {
+	   'all_albums' : all_albums,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail(request, album_id):
 	return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
