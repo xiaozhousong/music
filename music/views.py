ae4e0c0a3f7ea8c7c6615d8c6fc5eb1c04731306
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-13 13:05:23 +0000

    try get_object_or_404, ang it is working fine

--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -13,15 +13,9 @@ def index(request):
 	return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-	try:
-		album = Album.objects.get(pk =  album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk=album_id)
 	return render(request, 'music/detail.html', {'album' : album})
 
 def favorite(request, album_id):
-	try:
-		album = Album.objects.get(pk =  album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk=album_id)
 	return render(request, 'music/detail.html', {'album' : album})
