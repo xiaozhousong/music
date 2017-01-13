69171f63d97daf70a4ac1a7888e7a249bbb8fefd
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-13 13:15:19 +0000

    save select song to fav

--- music/views.py
+++ music/views.py
@@ -18,4 +18,14 @@ def detail(request, album_id):
 
 def favorite(request, album_id):
 	album = get_object_or_404(Album, pk=album_id)
-	return render(request, 'music/detail.html', {'album' : album})
+	try: 
+	    selected_song = album.song_set.get(pk=request.POST['song'])
+	except (KeyError, Song.DoesNotExist):
+		return render(request, 'music/detail.html', {
+			  'album' : album,
+			  'error_message': "You didnt select a valid song",
+			})
+	else:
+		selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'music/detail.html', {'album' : album})
