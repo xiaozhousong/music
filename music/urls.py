fc54176b1746e0f26f7f992dc59baf334e93e01b
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-21 18:17:31 +0000

    Update and delete class and edited the view

--- music/urls.py
+++ music/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
 
     #/music/album/add
     url(r'album/add/$', views.AlbumCreat.as_view(), name='album_add'),
+
+    #/music/album/music_id
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album_update'),
+
+    #/music/album/music_id/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album_delete'),
 ]
