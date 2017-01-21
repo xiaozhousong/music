f7b5066854b6bcc810e5e56c42378f94747fe06d
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-21 13:47:19 +0000

    define the object_list name with all_albums

--- music/views.py
+++ music/views.py
@@ -3,6 +3,7 @@ from .models import Album
 
 class IndexView(generic.ListView):
 	template_name = 'music/index.html'
+	context_object_name='all_albums'
 
 	def get_queryset(self):
 		return Album.objects.all()
