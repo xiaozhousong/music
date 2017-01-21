7528f99f3020469293293eb3f6ea2d4f8fb0fd3e
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-21 17:28:23 +0000

    Import reverse to model form and create form view to add a album

--- music/models.py
+++ music/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -9,6 +9,9 @@ class Album(models.Model):
 	album_title = models.CharField(max_length=500)
 	genre = models.CharField(max_length=100)
 	album_logo = models.CharField(max_length=1000)
+
+	def get_absolute_url(self):
+		return reverse('music:detail', kwargs={'pk':self.pk})
 
 	def __str__(self):
 		return self.album_title + ' - ' + self.artist
--- music/urls.py
+++ music/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
 
     #/music/music_id
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    #/music/album/add
+    url(r'album/add/$', views.AlbumCreat.as_view(), name='album_add'),
 ]
