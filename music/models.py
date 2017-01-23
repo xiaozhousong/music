e372e283f75af6e4bb97e56e8be1e64921b81e00
Xiaozhou Song <u1476165@unimail.hud.ac.ukk>
2017-01-23 17:39:34 +0000

    upload file to media

--- music/models.py
+++ music/models.py
@@ -2,13 +2,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
 
+
 # Create your models here.
 
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=500)
 	genre = models.CharField(max_length=100)
-	album_logo = models.CharField(max_length=1000)
+	album_logo = models.FileField()
 
 	def get_absolute_url(self):
 		return reverse('music:detail', kwargs={'pk':self.pk})
